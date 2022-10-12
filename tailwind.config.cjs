56a868b0b4bb1f21ae87e5617a0ff50246d67fbc
Gabriel de Lima Paes <gabriel.2paes@hotmail.com>
2022-10-12 12:42:35 -0300

    Importação dos tokens (Figma)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,25 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+        xs: 14,
+        sm: 16,
+        md: 18,
+        lg: 20,
+        xl: 24,
+        '2xl': 32,
+    },
+
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1E6',
+
+      'sun-500': '#FCCA46',
+      'sun-300': '#F8D16A'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
